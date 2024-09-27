f89578d5e76b48eb84e0d95f2c3f491d5b20fca8
reshav1234 <thapareshav50@gmail.com>
2024-09-28 00:47:15 +0545

    App updated

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,6 @@
-<<<<<<< Updated upstream
-import './App.css'
+
 import Login from './Pages/Login'
+import Dashboard from "./Components/Dashboard/Dashboard"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './Pages/Register';
 
@@ -11,20 +11,12 @@ function App() {
       <Routes>
           <Route path="/" element={<Login/>} />
           <Route path="/register" element={<Register/>} />
+          <Route path = "/dashboard" element = {<Dashboard />} />
         </Routes>
       </Router>
       </>
     )
   }
-=======
-import Dashboard from "./Components/Dashboard/Dashboard"
-const App = () => {
-  return(
-    <>
-      <Dashboard/>
-    </>
-  ) 
-}
->>>>>>> Stashed changes
+
 
 export default App
