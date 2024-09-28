f07c996b174817c70cbf9cdd25d0867ce665cad3
PrA-GYaN <pragyan1516@gmail.com>
2024-09-28 08:47:33 +0545

    a

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Router>
       <Routes>
           <Route path="/" element={<Landing/>} />
-          <Route path="/" element={<Login/>} />
+          <Route path="/login" element={<Login/>} />
           <Route path="/register" element={<Register/>} />
           <Route path = "/dashboard" element = {<Dashboard />} />
         </Routes>
