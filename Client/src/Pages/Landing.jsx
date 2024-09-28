0d1ad8f701c80fac1645367a03e6d1bcd1415cec
PrA-GYaN <pragyan1516@gmail.com>
2024-09-28 10:10:23 +0545

    Landing Page

--- Client/src/Pages/Landing.jsx
+++ Client/src/Pages/Landing.jsx
@@ -1,15 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Css/landing.css';
+import phone from '../../public/images/phone.png';
 
 const Landing = () => {
   return (
-    <>
-        <div className="landing-container">
-            <h1>Welcome to Money in the Bank!</h1>
-            <p>This is a simple banking application built with React.</p>
-            <p>Please log in or register to get started.</p>
-        </div>
-    </>
-  )
-}
+    <div className="landing">
+      <header className="landing-header">
+        <nav className="landing-nav">
+          <div className='left-section'>
+            <ul>
+              <li><Link to="#">Home</Link></li>
+              <li><Link to="#">Services</Link></li>
+              <li><Link to="#">Help</Link></li>
+              <li><Link to="#">Contact</Link></li>
+            </ul>
+            <div className="brand">MiTB</div>
+          </div>
 
-export default Landing
+          <div className="auth-buttons">
+            <Link to="/login">
+              <button className="sign-in">Sign in</button>
+            </Link>
+            <Link to="/register">
+              <button className="sign-up">Sign Up</button>
+            </Link>
+          </div>
+        </nav>
+      </header>
+
+      <main className="landing-main">
+        <div className="main-text">
+          <p className="platform">E-money platform to make</p>
+          <div className='slogan'>Split Bill Not<br/>Friendship</div>
+        </div>
+        <div className="main-img">
+            <div className="green-shape"></div>
+            <img src={phone} className='image'></img>
+        </div>
+      </main>
+
+      <div className="landing-buttons">
+        <Link to="#">
+          <button className="quick-payment">Quick Payment</button>
+        </Link>
+        <Link to="#">
+          <button className="user-activity">User Activity</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
